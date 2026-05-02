b74fa47c9f063cfb72c9f4b10e33e2aaa245833b
Prakash Babu paramhans <prakashbabuparamhans5g@gmail.com>
2026-05-02 13:20:44 +0000

    esh web app ko mobile , laptop, tablets, etc. me logo ke sath bilkul app

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,9 @@
-// Vikas Setu Service Worker
+
 const CACHE_NAME = 'vikas-setu-v1';
 const urlsToCache = [
   '/',
-  '/manifest.webmanifest'
+  '/manifest.webmanifest',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap'
 ];
 
 self.addEventListener('install', (event) => {

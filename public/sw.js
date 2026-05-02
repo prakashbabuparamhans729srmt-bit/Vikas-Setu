c724ce6c5891e5f0e6396224e19465e754217c29
Prakash Babu paramhans <prakashbabuparamhans5g@gmail.com>
2026-05-02 13:22:55 +0000

    esh web app ko mobile , laptop, tablets, etc. me logo ke sath bilkul app

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,24 @@
 
-const CACHE_NAME = 'vikas-setu-cache-v1';
+const CACHE_NAME = 'vikas-setu-core-v1';
 const urlsToCache = [
   '/',
-  '/manifest.webmanifest',
-  '/globals.css'
+  '/manifest.webmanifest'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
